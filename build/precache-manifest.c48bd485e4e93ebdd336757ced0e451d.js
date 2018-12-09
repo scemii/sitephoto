416611f29c83cad34226402058374311f1d0d5c1
scemii <pierre_raffalli@msn.com>
2018-12-09 15:10:40 +0100

    création du build folder

---
+++ build/precache-manifest.c48bd485e4e93ebdd336757ced0e451d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00dfb280f99d8d17bba4",
+    "url": "/static/css/main.4e039b0b.chunk.css"
+  },
+  {
+    "revision": "00dfb280f99d8d17bba4",
+    "url": "/static/js/main.00dfb280.chunk.js"
+  },
+  {
+    "revision": "3e8717721f08fed25cad",
+    "url": "/static/js/1.3e871772.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ca9f43ce249b9421226f6ab4656cddb",
+    "url": "/index.html"
+  }
+];
